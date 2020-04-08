77eb11f254b64c41b5d3d827857720b4f47dc040
Jason Kraus <zbyte64@gmail.com>
2020-04-07 20:31:21 -0700

    dry/optimize vectorize projections with precompute key strings

--- fuzzdom/data.py
+++ fuzzdom/data.py
@@ -56,6 +56,17 @@ def vectorize_projections(
     data = {}
     edges = []
     index = -1
+    # precompute key strings
+    final_domain_key = f"{final_domain}_index"
+    source_domain_key = f"{source_domain}_index"
+    sp_keys = [
+        {
+            "sp_domain_index": f"{source_domain}_{p_domain}_index",
+            "p_domain_index": f"{p_domain}_index",
+            "p_domain_value": f"{p_domain}_value",
+        }
+        for p_domain in proj_domains
+    ]
 
     for k, p in enumerate(combinations):
         # value w/ enum: [ (0, x0), (1, x1) ]
@@ -65,25 +76,17 @@ def vectorize_projections(
         for u, src_node in source.nodes(data=True):
             index += 1
             node_index = src_node["index"]
-            node = {
-                "index": index,
-                f"{final_domain}_index": k,
-                f"{source_domain}_index": node_index,
-            }
-            for src_index, (proj_index, proj_value) in enumerate(p):
-                p_domain = proj_domains[src_index]
-                node[f"{source_domain}_{p_domain}_index"] = (proj_index + 1) * (
-                    node_index + 1
-                ) - 1
-                node[f"{p_domain}_index"] = proj_index
+            node = {"index": index, final_domain_key: k, source_domain_key: node_index}
+            for keys, (proj_index, proj_value) in zip(sp_keys, p):
+                node[keys["sp_domain_index"]] = (proj_index + 1) * (node_index + 1) - 1
+                node[keys["p_domain_index"]] = proj_index
                 if isinstance(proj_value, dict):
                     node.update(proj_value)
                 elif proj_value is not None:
-                    node[f"{p_domain}_value"] = proj_value
+                    node[keys["p_domain_value"]] = proj_value
             for key, value in node.items():
                 data[key] = [value] if index == 0 else data[key] + [value]
 
-    # return dst, combinations
     for key, item in data.items():
         try:
             data[key] = torch.tensor(item)
@@ -93,12 +96,13 @@ def vectorize_projections(
     data["edge_index"] = torch.cat(edges).view(2, -1)
 
     indexes = {
-        f"{p_domain}_index": len(entries) for p_domain, entries in projections.items()
+        keys["p_domain_index"]: len(entries)
+        for keys, (p_domain, entries) in zip(sp_keys, projections.items())
     }
-    indexes[f"{final_domain}_index"] = len(combinations)
-    indexes[f"{source_domain}_index"] = source.number_of_nodes()
-    for p_domain, p_values in projections.items():
-        indexes[f"{source_domain}_{p_domain}_index"] = num_nodes * len(p_values)
+    indexes[final_domain_key] = len(combinations)
+    indexes[source_domain_key] = source.number_of_nodes()
+    for keys, (p_domain, p_values) in zip(sp_keys, projections.items()):
+        indexes[keys["sp_domain_index"]] = num_nodes * len(p_values)
 
     data = SubData(data, **indexes)
     data.num_nodes = num_nodes * len(combinations)
